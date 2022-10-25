c697eb107c261fb7b71219513c221e90b625da08
code-ry <rybussey@gmail.com>
2022-10-25 11:48:23 +1000

    DELETE and PATCH added to MVC

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
+from db import db, ma
+from controllers.cards_controller import cards_bp
 import os
 
 def create_app():
     app = Flask(__name__)
 
+    @app.errorhandler(404)
+    def not_found(err):
+        # return the str of the object returned
+        return {"error" : str(err)}, 404
+
+    app.config['JSON_SORT_KEYS'] = False
     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+
+    db.init_app(app)
+    ma.init_app(app)
+
+    app.register_blueprint(cards_bp)
+
+    return app
